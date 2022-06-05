12285ed5a64bec7439b0108d9bea0144ab34e512
Will P <96395120+mayowill303@users.noreply.github.com>
2022-06-04 19:09:49 -0500

    Dashboard App Update

--- app/start.sh
+++ app/start.sh
@@ -1,1 +1,7 @@
-
+. venv/bin/activate
+export FLASK_APP=app.py
+export SQLALCHEMY_DATABASE_URI="postgresql://postgres:<db_password>@127.0.0.1:5432/freeze_analysis"
+export SECRET_KEY="sasdasdasdasdafwq"
+export PYTHONPATH=.
+export FLASK_ENV=development
+flask run
