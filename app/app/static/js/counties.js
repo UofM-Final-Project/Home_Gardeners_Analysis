ab10ab56f3fa790a4798aaac34da7e6258bdfb97
Will P <96395120+mayowill303@users.noreply.github.com>
2022-06-06 17:58:34 -0500

    Create counties.js

---
+++ app/app/static/js/counties.js
@@ -0,0 +1,44 @@
+// An array containing each county's location, state, and average freeze date.
+let counties = [{
+    location: [40.167950, -92.358429],
+    county: "Hennepin County",
+    state: "MN",
+    avg_last_freeze_dayofyear: 124.0
+  },
+  {
+    location: [40.717869, -86.278923],
+    county: "Anoka County",
+    state: "MN",
+    avg_last_freeze_dayofyear: 131.0
+  },
+  {
+    location: [30.802510, -82.870760],
+    County: "Carver County",
+    State: "MN",
+    avg_last_freeze_dayofyear: 122.0
+  },
+  {
+    location: [44.735291, -118.2437],
+    County: "Dakota County",
+    State: "MN",
+    avg_last_freeze_dayofyear: 123.0
+  },
+  {
+    location: [48.006770, -98.552610],
+    county: "Ramsey County",
+    state: "MN",
+    avg_last_freeze_dayofyear: 120.0
+  }
+  {
+    location: [32.228271, -89.471764],
+    county: "Scott County",
+    state: "MN",
+    avg_last_freeze_dayofyear: 118.0
+  }
+  {
+    location: [36.728722, -81.921509],
+    county: "Washington County",
+    state: "MN",
+    avg_last_freeze_dayofyear: 114.0
+  }
+  ];
